876ab34f2c10adb5b0fe3fe7a95aace7c344f40b
Davide Gessa <gessadavide@gmail.com>
2017-07-19 17:28:51 +0200

    test migrated to Hex

--- test/test.ml
+++ test/test.ml
@@ -1,42 +1,32 @@
 open Bitcoinml;;
 open Stdint;;
 open OUnit2;;
-
-let ints_to_bytes il = 
-	let (^$) s c = s ^ String.make 1 c in
-	List.fold_left (^$) "" (List.map Char.chr il) 
-;;
-
-let ints_to_bytes_test octx = 
-	ints_to_bytes [ 0x63; 0x69; 0x61; 0x6f ] |> assert_equal "ciao"
-;;
+open Hex;;
 
 let base58_encode_check_test octx =
-	let adr = ints_to_bytes [ 0x00; 0x01; 0x09; 0x66; 0x77; 0x60; 0x06; 0x95; 0x3D; 
-		0x55; 0x67; 0x43; 0x9E; 0x5E; 0x39; 0xF8; 0x6A; 0x0D; 0x27; 
-		0x3B; 0xEE; 0xD6; 0x19; 0x67; 0xF6 ] in
+	let adr = Hex.to_string (`Hex "00010966776006953D5567439E5E39F86A0D273BEED61967F6") in
 	let adrb58 = Base58.encode_check adr in
 	assert_equal adrb58 "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM"
 ;;
 
 let varint_parse_test bl res octx = 
-	let v1 = Varint.parse_varint (Bitstring.bitstring_of_string (ints_to_bytes bl)) in
+	let v1 = Varint.parse_varint (Bitstring.bitstring_of_string (Hex.to_string bl)) in
 	assert_equal (fst v1) (Uint64.of_int res)
 ;;
 
 let varint_serialize_test bl res octx = 
 	let l = Varint.bitstring_of_varint (Int64.of_int res) in
-	assert_equal (Bitstring.string_of_bitstring l) (ints_to_bytes bl)
+	assert_equal (Bitstring.string_of_bitstring l) (Hex.to_string bl)
 ;;
 
 let script_parse_test sc scdec octx = 
-	let s = Script.parse (ints_to_bytes sc) in
+	let s = Script.parse (Hex.to_string sc) in
 	assert_equal (fst s) (fst scdec)
 ;;
 
 let script_serialize_test sc scdec octx =
 	let s = Script.serialize scdec in
-	assert_equal (ints_to_bytes sc) s
+	assert_equal (Hex.to_string sc) s
 ;;
 
 let script_is_spendable_test sc octx =
@@ -49,39 +39,32 @@ let script_spendable_by_test sc adr octx =
 ;;
 
 let suite = "bitcoinml" >::: [
-	"helper.ints_to_bytes"	>:: ints_to_bytes_test;
 	"base58.encode_check" 	>:: base58_encode_check_test;
-	"varint.parse" 			>:: varint_parse_test [0x16] 0x16;
-	"varint.parse2" 		>:: varint_parse_test [0xFE; 0x32; 0x32; 0x32; 0x32] 0x32323232;
-	(*"varint.parse3" 		>:: varint_parse_test [0xFF; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32] 0x3232323232323232;*)
-	"varint.serialize"		>:: varint_serialize_test [0x16] 0x16;
-	"varint.serialize2" 	>:: varint_serialize_test [0xFE; 0x32; 0x32; 0x32; 0x32] 0x32323232;
-	(*"varint.serialize3"	>:: varint_serialize_test [0xFF; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32; 0x32] 0x3232323232323232;*)
-	"script.parse" 			>:: script_parse_test [
-			0x76; 0xA9; 0x14; 0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0x88; 0xAC 
-		]
+	"varint.parse" 			>:: varint_parse_test (`Hex "16") 0x16;
+	"varint.parse2" 		>:: varint_parse_test (`Hex "FE32323232") 0x32323232;
+	(*"varint.parse3" 		>:: varint_parse_test (`Hex "FF3232323232323232") 0x3232323232323232;*)
+	"varint.serialize"		>:: varint_serialize_test (`Hex "16") 0x16;
+	"varint.serialize2" 	>:: varint_serialize_test (`Hex "FE32323232") 0x32323232;
+	(*"varint.serialize3"	>:: varint_serialize_test (`Hex "FF3232323232323232") 0x3232323232323232;*)
+	"script.parse" 			>:: script_parse_test (`Hex "76A91489ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA88AC")
 		([
-			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, ints_to_bytes [0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA]); Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
+			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, Hex.to_string (`Hex "89ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA")); 
+			Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
 		], 25);
-	"script.serialize" 		>:: script_serialize_test [
-			0x76; 0xA9; 0x14; 0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0x88; 0xAC 
-		]
+	"script.serialize" 		>:: script_serialize_test (`Hex "76A91489ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA88AC")
 		([
-			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, ints_to_bytes [0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA]); Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
+			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, Hex.to_string (`Hex "89ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA")); 
+			Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
 		], 25);
 	"script.is_spendable"	>:: script_is_spendable_test 
 		([
-			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, ints_to_bytes [0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA]); Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
+			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, Hex.to_string (`Hex "89ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA")); 
+			Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
 		], 25);
 	"script.spendable_by"	>:: script_spendable_by_test 
 		([
-			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, ints_to_bytes [0x89; 0xAB; 0xCD; 0xEF; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 
-			0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA; 0xAB; 0xBA]); Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
+			Script.OP_DUP; Script.OP_HASH160; Script.OP_DATA (20, Hex.to_string (`Hex "89ABCDEFABBAABBAABBAABBAABBAABBAABBAABBA")); 
+			Script.OP_EQUALVERIFY; Script.OP_CHECKSIG
 		], 25) 
 		"1DYwPTpZuLjY2qApmJdHaSAuWRvEF5skCN";
 ];;
