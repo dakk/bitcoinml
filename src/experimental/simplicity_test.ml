ca94d36fdaaa7288c133be1c216dd3861bce5da7
Davide Gessa <gessadavide@gmail.com>
2017-10-31 22:41:48 +0100

    simplicity tests

--- src/experimental/simplicity_test.ml
+++ src/experimental/simplicity_test.ml
@@ -1,58 +1,81 @@
-type __ = Obj.t
-
-type unit0 =
-| Tt
+type unit0 = | Tt;;
 
 type ('a, 'b) sum =
 | Inl of 'a
 | Inr of 'b
+;;
 
-type ('a, 'b) prod =
-| Pair of 'a * 'b
+type ('a, 'b) prod = | Pair of 'a * 'b;;
 
-(** val fst : ('a1, 'a2) prod -> 'a1 **)
+let fst = function | Pair (x, _) -> x;;
 
-let fst = function
-| Pair (x, _) -> x
-
-(** val snd : ('a1, 'a2) prod -> 'a2 **)
-
-let snd = function
-| Pair (_, y) -> y
+let snd = function | Pair (_, y) -> y;;
 
 type ty =
 | Unit
 | Sum of ty * ty
 | Prod of ty * ty
-
-type tySem = __
+;;
 
 type term =
-| Iden of ty
-| Comp of ty * ty * ty * term * term
-| Unit0 of ty
-| Injl of ty * ty * ty * term
-| Injr of ty * ty * ty * term
-| Case of ty * ty * ty * ty * term * term
-| Pair0 of ty * ty * ty * term * term
-| Take of ty * ty * ty * term
-| Drop of ty * ty * ty * term
+| Iden
+| Comp of term * term
+| Unit
+| Injl of term
+| Injr of term
+| Case of term * term
+| Pair0 of term * term
+| Take of term
+| Drop of term
+;;
 
-(** val eval : ty -> ty -> term -> tySem -> tySem **)
-
-let rec eval _ _ x a =
+let rec eval x a =
   match x with
-  | Iden _ -> a
-  | Comp (a0, b, c, s, t) -> eval b c t (eval a0 b s a)
-  | Unit0 _ -> Obj.magic Tt
-  | Injl (a0, b, _, t) -> Obj.magic (Inl (eval a0 b t a))
-  | Injr (a0, _, c, t) -> Obj.magic (Inr (eval a0 c t a))
-  | Case (a0, b, c, d, s, t) ->
+  | Iden -> a
+  | Comp (s, t) -> eval t (eval s a)
+  | Unit -> Obj.magic Tt
+  | Injl (t) -> Obj.magic (Inl (eval t a))
+  | Injr (t) -> Obj.magic (Inr (eval t a))
+  | Case (s, t) ->
     let Pair (ab, c0) = Obj.magic a in
     (match ab with
-     | Inl a1 -> eval (Prod (a0, c)) d s (Obj.magic (Pair (a1, c0)))
-     | Inr b0 -> eval (Prod (b, c)) d t (Obj.magic (Pair (b0, c0))))
-  | Pair0 (a0, b, c, s, t) ->
-    Obj.magic (Pair ((eval a0 b s a), (eval a0 c t a)))
-  | Take (a0, _, c, t) -> eval a0 c t (fst (Obj.magic a))
-  | Drop (_, b, c, t) -> eval b c t (snd (Obj.magic a))
+     | Inl a1 -> eval s (Obj.magic (Pair (a1, c0)))
+     | Inr b0 -> eval t (Obj.magic (Pair (b0, c0))))
+  | Pair0 (s, t) ->
+    Obj.magic (Pair ((eval s a), (eval t a)))
+  | Take (t) -> eval t (fst (Obj.magic a))
+  | Drop (t) -> eval t (snd (Obj.magic a))
+
+
+
+module Examples = struct
+  let two = Sum (Unit, Unit);;
+  let not = Comp (
+    (Pair0 (Iden, Unit)),
+    (Case (Injr Unit, Injl Unit))
+  );;
+
+  let halfadder = Case (
+    Drop (Pair0 (Injl (Unit), Iden)),
+    Drop (Pair0 (Iden, not))
+  );;
+
+  let fulladder1 = 
+    Comp (
+      Pair0 (Take halfadder, Drop Iden),
+      Comp ( 
+        Pair0 (
+          Take (Take Iden),
+          Comp (
+            Pair0 (Take (Drop Iden), Drop Iden),
+            halfadder
+          )
+        ),
+        Pair0 (
+          Case (Injr Unit, Drop (Take Iden)),
+          Drop (Drop Iden)
+        )
+      )
+    )
+  ;;
+end
